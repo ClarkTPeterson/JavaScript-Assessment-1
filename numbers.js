b487aa5c3631d39ee7540545d4426aa39c9da4f2
Clark Peterson <clark.t.peterson@gmail.com>
2015-09-08 12:22:24 -0600

    numbers 1 and 3 done

--- numbers.js
+++ numbers.js
@@ -2,10 +2,18 @@
 var num10 = 10;
 var string8 = '8';
 var one = 1;
-//2. write a loop that will log only numbers divisible by 3 between 20 - 100
+var sum = num10+string8+one;
+sum;
 
+//2. write a loop that will log only numbers divisible by 3 between 20 - 100
 
 
 //3. find the class average given the following test scores
 
 var scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98]; 
+var sum = 0;
+for(var i = 0;i<scores.length;i++){
+    sum = sum + scores[i];
+}
+sum = sum/scores.length;
+sum;
