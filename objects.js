ee10c3d1bec8b4f9d3d083b8344e6df4eb4cfe01
Clark Peterson <clark.t.peterson@gmail.com>
2015-09-09 19:14:51 -0600

    objects done

--- objects.js
+++ objects.js
@@ -1,9 +1,14 @@
 //1. Using dot and bracket notation modify the existing object values to replace the information with your own
 
 var me = {
-	'name': 'Jake Overall',
-	'age': 29,
-	'hair color': 'brown'
+	name:'Jake Overall',
+	age: 29,
+	hairColor:'brown'
 };
+
+me.name='Clark Peterson';
+me.age=30;
+
+me;
 
 //2. Iterate over the object to console.log the propery or key names. 
